f6f86eef019e16fb2f4e1242a5b35e7b3b5c3fa1
thuanpm <thuanpm@sunshinegroup.vn>
2022-04-14 17:29:06 +0700

    fix autho and add example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## 0.0.2
+
+- fix Authorize and add example
+
+## 0.0.2
+
+- fix constructor
+
 ## 0.0.1
 
-- TODO: init packages
+- init packages
---
+++ example/main.dart
@@ -0,0 +1,59 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/material.dart';
+import 'package:sign_with_unicloud_id/sign_with_unicloud_id.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final SignWithUnicloudId signWithUnicloudId;
+
+  @override
+  void initState() {
+    signWithUnicloudId = SignWithUnicloudId.instance;
+    signWithUnicloudId.setConfig(redirectUrl: 'vn.unicloudid.auth');
+    super.initState();
+  }
+
+  Future<void> _signInWithAutoCodeExchange() async {
+    try {
+      var ret = await signWithUnicloudId.authorize();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+            child: Container(
+          child: Platform.isIOS
+              ? Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    child: const Text(
+                      'Sign in with Unicloud ID(iOS only)',
+                      textAlign: TextAlign.center,
+                    ),
+                    onPressed: _signInWithAutoCodeExchange,
+                  ),
+                )
+              : ElevatedButton(
+                  child: const Text('Sign in with Unicloud ID'),
+                  onPressed: () => _signInWithAutoCodeExchange(),
+                ),
+        )),
+      ),
+    );
+  }
+}
